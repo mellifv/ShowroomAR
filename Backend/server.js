290540c05e400fac86d2aa88b5dd76f29a67d4cb
mellifv <124873001+mellifv@users.noreply.github.com>
2025-11-01 22:26:22 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,9 +11,15 @@ import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors({
+  origin: [
+    'https://showroom-ar-91by.vercel.app',  // Your Vercel frontend
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
